9e4b75487a0b603e4c54389bdbb43dbcd30c117a
HuizeMao <38351685+HuizeMao@users.noreply.github.com>
2020-04-11 14:31:13 +0200

    OI template

---
+++ template.cpp
@@ -0,0 +1,23 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+inline int read() 
+{//change type of var to read as needed
+	int f=0,sign=1;char ch=getchar();
+	while(!isdigit(ch)) {if(ch=='-')sign=-1;ch=getchar();}
+	while(isdigit(ch)) {f=f*10+ch-48,ch=getchar();}
+	return sign*f;
+}
+
+int main() {
+    int casos=read();
+    while(casos--)
+    {
+    	
+	}
+    return 0;
+}
